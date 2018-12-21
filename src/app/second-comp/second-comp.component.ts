10bd6c0dc3a73af6228cf321a6a9541ae5564583
shivkore <kores9214@gmail.com>
2018-12-22 00:38:26 +0530

    Assignment43 added

---
+++ src/app/second-comp/second-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-comp',
+  templateUrl: './second-comp.component.html',
+  styleUrls: ['./second-comp.component.css']
+})
+export class SecondCompComponent {
+
+}
